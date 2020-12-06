00a9d4208ea450ec6ba9befe256127ed6d96cada
xgit <goer99@users.noreply.github.com>
2020-12-06 16:51:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,19 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 将build日期写入GRUB TITLE
+cat >> .config <<EOF
+#CONFIG_KERNEL_BUILD_DOMAIN="Tom on $(date)"
+#CONFIG_KERNEL_BUILD_USER="Tom on $(date)"
+CONFIG_GRUB_TITLE="Lean OpenWrt Stable AutoBuild by Tom on $(date)"
+EOF
+
+# 更改默认ip
+# sed -i 's/192.168.1.1/新的ip/g' package/base-files/files/bin/config_generate
+
+# 更改主机名
+#sed -i "s/OpenWrt/新主机名/g" package/base-files/files/bin/config_generate
+
+# 更改版本号
+sed -i "s/OpenWrt/Tom stable build on $(date -I) for /g" package/lean/default-settings/files/zzz-default-settings
